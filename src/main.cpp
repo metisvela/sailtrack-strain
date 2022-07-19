5bffa4c5f0d5cd545656570780f9d94ee0ded908
gianlucarossi15 <gianluca.rossi2000@live.com>
2022-07-20 01:12:07 +0200

    Create read_data function

--- src/main.cpp
+++ src/main.cpp
@@ -18,17 +18,23 @@
 // TODO: Adjust value
 //taken from grafana, so no more
 //#define LOADCELL_BASE_LOAD_KG		36.1 
-#define LOADCELL_NUM_READING		32
-//divider:1241.496
-
+#define LOADCELL_NUM_READING		10
+//passed by grafana if diffent
+#define DEFAULT_TIME				30000
+#define g							9.80
 #define LOOP_TASK_INTERVAL_MS		1000 / MQTT_PUBLISH_FREQ_HZ
 
+//WEIGHT= (val-OFFSET)/DIVIDER
 // ------------------------------------------------------------------- //
 
 SailtrackModule stm;
 //libreria per la comunicazione tra codice-amp-cella_di_carico
 HX711 hx;
-
+bool cal_status=false;
+int cal_time;
+int load_kg;
+void read_data();
+void calibration(int load_kg=0, int cal_time=DEFAULT_TIME);
 class ModuleCallbacks: public SailtrackModuleCallbacks {
 	void onStatusPublish(JsonObject status) {
 		JsonObject battery = status.createNestedObject("battery");
@@ -39,47 +45,82 @@ class ModuleCallbacks: public SailtrackModuleCallbacks {
 		}
 		battery["voltage"] = 2 * avg / BATTERY_ADC_RESOLUTION * BATTERY_ESP32_REF_VOLTAGE * BATTERY_ADC_REF_VOLTAGE;
 	}
+	//parso il json per prendere peso e tempo per la calibrazione
+	/*void onMqttMessage(const char * topic,JsonObject payload) {
+		if(strcmp(topic,"sensor/strain0/calibration")){
+			stm.subscribe("sensor/strain0/calibration");
+			StaticJsonDocument<STM_JSON_DOCUMENT_MEDIUM_SIZE> doc;
+			deserializeJson(doc,payload);
+			//come sono i mex mqtt??
+			load_kg=doc["load"];
+			cal_time=doc["time"];
+			cal_status=true;
+		}
+	}*/
+
 };
 
 
 void setup() {
-	stm.begin("strain", IPAddress(192, 168, 42, 105), new ModuleCallbacks());
-	
+	stm.begin("strain", IPAddress(192, 168, 1, 10), new ModuleCallbacks());
 }
 
 void loop() {
-	calibration(36.1);
-	
-}
+	if(cal_status)
+		calibration(36.1,60);
+	else
+		//altrimenti leggo i dati dall'amp e li spedisco nel relativo topic
+		read_data();
 
-void calibration(int load_kg){
-	int calibration_factor,load;
+}
+void read_data(){	
 	TickType_t lastWakeTime = xTaskGetTickCount();
 	StaticJsonDocument<STM_JSON_DOCUMENT_MEDIUM_SIZE> doc;
+	long reading = hx.get_units(LOADCELL_NUM_READING);
+	doc["raw_data"]=reading;
+	stm.publish("sensor/strain0", doc.as<JsonObjectConst>());	
+	vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(LOOP_TASK_INTERVAL_MS));
+}
+void calibration(int load_kg, int cal_time){
+	TickType_t lastWakeTime = xTaskGetTickCount();
+	int divider,load;
+	long reading, offset;	
+	StaticJsonDocument<STM_JSON_DOCUMENT_MEDIUM_SIZE> cal;
 	hx.begin(HX711_DOUT_PIN, HX711_SCK_PIN);
 	hx.set_scale();
-	//set offset a tare weight
 	hx.tare();
+	//offset is the average value now measured
+	offset=hx.get_units(LOADCELL_NUM_READING);
 	//initiate calibration
 	digitalWrite(STM_NOTIFICATION_LED_PIN, LOW);
-	//30 seconds to attach the load cell shroud
-	delay(30000);
+
+	//time to attach the loadcell to the shroud
+	delay(cal_time);
 	digitalWrite(STM_NOTIFICATION_LED_PIN, HIGH);
 	//takes the average based on 10 reading
-	long reading = hx.get_units(10);
-	if(load_kg==0)
+	reading = hx.get_units(LOADCELL_NUM_READING);
+	if(load_kg==0){
 		load=0;
+		offset=0;
+		divider=0;
+	}
 	else
 	{
-	calibration_factor=reading/load_kg;
-	log_printf("Calibration factor: %ld/%.1f=%.3f\n", reading, load_kg, calibration_factor);
-	hx.set_scale(calibration_factor);
-	load=reading/calibration_factor;
-	////////////////????????////////////////
-	//doc["load"] = val;
-	}
+	divider=reading/load_kg;
+	log_printf("Divider: %ld/%.1f=%.3f\n", reading, load_kg, divider);
+	hx.set_scale(divider);
+	load=reading-offset/divider;
+	}	
+	
+	//data measured by the loadcell
+	cal["raw_data"]=reading;
 	//it gives the weight measured
-	doc["load"]=load;
-	stm.publish("sensor/strain0", doc.as<JsonObjectConst>());
+	cal["load_kg"]=load;
+	cal["load_newton"]=load*g;
+	//calibration factor 
+	cal["divider"]=divider;
+	cal["offset"]=offset;
+	stm.publish("sensor/strain0/calibration", cal.as<JsonObjectConst>());
+	cal_status=false;	
 	vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(LOOP_TASK_INTERVAL_MS));
 }
