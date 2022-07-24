130cff42e19dea208fed8c095c220304de5849de
gianlucarossi15 <gianluca.rossi2000@live.com>
2022-07-24 13:46:38 +0200

    ADD read and write memory method

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,12 @@ SailtrackModule stm;
 //libreria per la comunicazione tra codice-amp-cella_di_carico
 HX711 hx;
 bool cal_status=false;
-int cal_time;
-int load_kg;
-long default_offset;
+
+double divider;
+long int offset;
+int address=0;
 void read_data();
-void calibration(int load_kg=0, int cal_time=DEFAULT_TIME);
+void calibration(double in_load=0, int cal_time=DEFAULT_TIME);
 class ModuleCallbacks: public SailtrackModuleCallbacks {
 	void onStatusPublish(JsonObject status) {
 		JsonObject battery = status.createNestedObject("battery");
@@ -66,29 +67,30 @@ class ModuleCallbacks: public SailtrackModuleCallbacks {
 
 
 void setup() {
-	stm.begin("strain", IPAddress(192, 168, 42, 105), new ModuleCallbacks());
+	stm.begin("strain", IPAddress(192, 168, 1, 243), new ModuleCallbacks());	
+	EEPROM.begin(EEPROM_SIZE);
 	//stm.subscribe("sensor/strain0/calibration");
 	hx.begin(HX711_DOUT_PIN,HX711_SCK_PIN);	
-	//iscrizione serve solo per la ricezione di dati
-	
+	//iscrizione serve solo per la ricezione di dati	
+	Serial.begin(115200);	
+	Serial.println("setup");
 	//default_offset=hx.get_units(LOADCELL_NUM_READING);
+	EEPROM.get(address,offset);
+	address+=sizeof(offset);
+	EEPROM.get(address,divider);
+	address=0;
 }
 
 void loop() {
-	if(!cal_status){
-		calibration(36.1,60);
-		cal_status=true;
-	}
-	//else
-		//altrimenti leggo i dati dall'amp e li spedisco nel relativo topic
-		//read_data();
+	calibration(3);
+	
 }
 void read_data(){	
 	TickType_t lastWakeTime = xTaskGetTickCount();
 	StaticJsonDocument<STM_JSON_DOCUMENT_MEDIUM_SIZE> doc;
 	long reading = hx.get_units(LOADCELL_NUM_READING);
 	doc["raw_data"]=reading;
-	int load=(reading-default_offset)/DEFAULT_DIVIDER;
+	int load=(reading-0)/DEFAULT_DIVIDER;
 	//int load=(reading-reading)/DEFAULT_DIVIDER;
 	doc["load_kg"]=load;
 	doc["load_newton"]=load*g;
@@ -98,15 +100,18 @@ void read_data(){
 	vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(LOOP_TASK_INTERVAL_MS));
 }
 void writeMemory(){
-	EEPROM.begin(EEPROM_SIZE);
+	/*
+	address0: offset
+	address1: divider
+	*/
 }
-void calibration(int load_kg, int cal_time){
+void calibration(double in_load, int cal_time){
 	TickType_t lastWakeTime = xTaskGetTickCount();
-	int divider,load;
-	long reading, offset;	
+	double out_load;
+	long int reading;
 	StaticJsonDocument<STM_JSON_DOCUMENT_MEDIUM_SIZE> cal;
 	hx.set_scale();
-	hx.tare();
+	hx.tare(LOADCELL_NUM_READING);
 	//offset is the average value now measured
 	offset=hx.get_units(LOADCELL_NUM_READING);
 	//initiate calibration
@@ -117,24 +122,34 @@ void calibration(int load_kg, int cal_time){
 	digitalWrite(STM_NOTIFICATION_LED_PIN, HIGH);
 	//takes the average based on 10 reading
 	reading = hx.get_units(LOADCELL_NUM_READING);
-	if(load_kg==0){
-		load=0;
+	
+	if(in_load==0 || reading==0){
+		out_load=0;
 		offset=0;
 		divider=0;
+		Serial.println(offset);
+		Serial.println(reading);
+		Serial.println(divider);
+		Serial.println(out_load);
 	}
 	else
 	{
-	divider=reading/load_kg;
-	log_printf("Divider: %ld/%.1f=%.3f\n", reading, load_kg, divider);
+	divider=reading/in_load;
+	//log_printf("Divider: %ld/%.1f=%.3f\n", reading, load_kg, divider);
 	hx.set_scale(divider);
-	load=(reading-offset)/divider;
+	out_load=(reading-offset)/divider;
+	Serial.println(offset);
+	Serial.println(reading);
+	Serial.println(divider);
+	Serial.println(out_load);
+	Serial.println("fine calibrazione");
 	}	
 	
 	//data measured by the loadcell
 	cal["raw_data"]=reading;
 	//it gives the weight measured
-	cal["load_kg"]=load;
-	cal["load_newton"]=load*g;
+	cal["load_kg"]=out_load;
+	cal["load_newton"]=out_load*g;
 	//calibration factor 
 	cal["offset"]=offset;
 	cal["divider"]=divider;	
