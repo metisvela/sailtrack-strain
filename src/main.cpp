48caaf90d655a06e5b078b9743a3c0af81c85a59
gianlucarossi15 <gianluca.rossi2000@live.com>
2022-11-05 18:37:46 +0100

    Final version

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <SailtrackModule.h>
 #include <HX711.h>
 #include <EEPROM.h>
-#include <stdint.h>
 
 // -------------------------- Configuration -------------------------- //
 
@@ -40,12 +39,28 @@
 
 SailtrackModule stm;
 HX711 hx;
-
+/*
+	offset->tare
+	scale->divider
+*/
 bool calibration;
 int calibrationTries=CAL_NUM_TRIES;
 int calibrationScaleDelay;
 float calibrationLoad;
+/**
+ * @brief This class contains functions that will be called asynchronously by the SailTrack Module library.
+ * 
+ */
 class ModuleCallbacks: public SailtrackModuleCallbacks {
+	/**
+	 * @brief This function appends battery status to the other hardware informations provided by the Json 
+	 * status.
+	 * @details This function is called asynchronously by the SailTrack Module library when it necessary
+	 * to know battery information.
+	 * Battery information is calculated over BATTERY_NUM_READINGS readings to have a more robust value.
+	 * 
+	 * @param status is the Json object the stores hardware informations.
+	 */
 	void onStatusPublish(JsonObject status) {
 		JsonObject battery = status.createNestedObject("battery");
 		float avg = 0;
@@ -55,17 +70,45 @@ class ModuleCallbacks: public SailtrackModuleCallbacks {
 		}
 		battery["voltage"] = 2 * avg / BATTERY_ADC_RESOLUTION * BATTERY_ESP32_REF_VOLTAGE * BATTERY_ADC_REF_VOLTAGE;
 	}
-	//parso il json per prendere peso e tempo per la calibrazione
+	/**
+	 * @brief This function initiate the calibration procedure by setting load and time to attach 
+	 * the load cell to the shroud.
+	 * 
+	 * @details When the MQTT message has been sent, it calls asynchronously this function that starts the
+	 * calibration procedure.
+	 * By using MQTT protocol MQTT message is simply a Json file.
+	 * The sender of the message can provide two values:
+	 * load of the shroud->compulsory
+	 * time to attach the load cell to the shroud->not compulsory
+	 * If time is not provided, it will be used default value.
+	 * 
+	 * @param topic is the "scope" where the message has been published.
+	 * 
+	 * @param message is what has been sent via MQTT protocol
+	 */
 	void onMqttMessage(const char * topic, JsonObjectConst message) {
 		calibrationLoad=message["calibrationLoad"];
+		//calibrationScaleDelay is optional. If it has not been provided, it will be set to the default value CAL_SCALE_DELAY_S
 		calibrationScaleDelay=message["calibrationScaleDelay"]?message["calibrationScaleDelay"]:CAL_SCALE_DELAY_S;
 		calibration=true;
 	}
 
 };
-//to read
+/**
+ * @brief This function does the CYCLIC REDUNDANCY CHECK for the memory area that stores the offset and divider.
+ * 
+ * @details This function operates the incremental CYCLIC REDUNDANCY CHECK by using xor operations with 0xA001 polynomial.
+ * The remainder of the division is the CRC.
+ * 
+ * @param crc the actual remainder
+ * 
+ * @param a the payload over to operate the CRC.
+ * 
+ * @return uint16_t is the result of the CRC operation
+ */
 uint16_t crc16Update(uint16_t crc, uint8_t a) {
     int i;
+	//logic xor
     crc ^= a;
     for (i = 0; i < 8; i++) {
         if (crc & 1) crc = (crc >> 1) ^ 0xA001;
@@ -73,7 +116,20 @@ uint16_t crc16Update(uint16_t crc, uint8_t a) {
     }
     return crc;
 }
-//to read
+/**
+ * @brief It loads offset and divider value into amplifier system.
+ * 
+ * @details It loads offset and divider value into amplifier system.
+ * If the crc reveals some problems, the load calibration operation will not be performed.
+ * Before getting offset and divider values from memory, the algorithm checks whether the checksum result
+ * from offset and divider just read from memory and the checksum stored in memory is zero (calculated previously
+ * while saving values into memory).
+ * If the result is zero, it means that the data hadn't been corrupted.
+ * 
+ * @return true if the load of the calibration works properly.
+ * 
+ * @return false otherwise.
+ */
 bool loadCalibration() {
 	uint8_t buf[EEPROM_CAL_SIZE_BYTES];
 
@@ -82,7 +138,7 @@ bool loadCalibration() {
 		buf[a] = EEPROM.read(a + EEPROM_CAL_ADDR);
 		crc = crc16Update(crc, buf[a]);
 	}
-
+	//if the are data corrupted crc is not zero.
 	if (crc != 0 || buf[0] != EEPROM_CAL_ID_0 || buf[1] != EEPROM_CAL_ID_1) 
 		return false;	
 
@@ -98,10 +154,22 @@ bool loadCalibration() {
 	return true;
 }
 
-
+/**
+ * @brief This routine carries out the saving operation of the calibration.
+ * 
+ * @details this function stores offset and divider values into the EEPROM memory for future uses.
+ * It uses crc to check wheter the memory had been written before.
+ * After adding offset and divider values into the memory, the algoritm appends the checksum of those values
+ * to check while getting values from memory, if they had been modified or not.
+ * 
+ * @return true if the saving calibration operation works properly
+ * 
+ * @return false otherwise
+ */
 bool saveCalibration() {
 
 	uint8_t buf[EEPROM_CAL_SIZE_BYTES];
+	//set to zero EEPROM_CAL_SIZE_BYTES pointed by buf
 	memset(buf, 0, EEPROM_CAL_SIZE_BYTES);
 	buf[0] = EEPROM_CAL_ID_0;
 	buf[1] = EEPROM_CAL_ID_1;
@@ -127,19 +195,19 @@ bool saveCalibration() {
 
 /**
  * @brief This function set the tare and scale value based on the given load,
- * passed by the parameter calLoad
+ * passed by the parameter calLoad.
  * 
  * @details The function starts by checking whether the passed parameter is zero or not.
  * The first blinking carried out onto the onboard led, is necessary to tell the user
  * that the setting tare block is ongoing.
  * The second blinking is used for the setting scale procedure.
  * Tare blinking and scale blinking are performed at different speed.
- * Lastly, the two values are stored into the EEPROM memory of the ESP32 micro-controller.
+ * Lastly, the two values are stored into the EEPROM memory of the ESP32 microcontroller.
  * 
  * @param calLoad is the value used for calibrating the load cell.
  * 
- * @param calScaleDelay refers to the time necessary to attach the load cell.
- * to the shroud
+ * @param calScaleDelay refers to the time necessary to attach the load cell
+ * to the shroud.
  * 
  * @return true if the calibration process terminates correctly.
  * 
@@ -174,6 +242,7 @@ bool calibrate(float calLoad, int calScaleDelay){
  * 
  * @details This function creates a new Json object, useful to store the raw reading from
  * the load cell and the actual load calculated by the load cell.
+ * The raw reading is the average of HX711_NUM_READING values.
  * At the end the Json object is published in the sensor/strain0 topic. 
  */
 void readData(){	
@@ -185,14 +254,14 @@ void readData(){
 	vTaskDelayUntil(&lastWakeTime, pdMS_TO_TICKS(LOOP_TASK_INTERVAL_MS));
 }
 /**
- * @brief brief description Setup function is used to initiliaze the system.
+ * @brief Setup function is used to initiliaze the system.
  * 
  * @details The following procedures are executed:
- * The operation amplifier are set with the hx.begin() function.
+ * The system amplifier is initiated with the hx.begin() function.
  * EEPROM size used to store the data is set with EEPROM.begin().
  * The system loads the previous calibration stored in the EEPROM memory.
  * It subscribes to the sensor/strain0/calibration topic,
- *  to be able to receive from that topic.
+ *  to be able to receive MQTT messages from that topic.
  */
 void setup() {
 	stm.begin("strain", IPAddress(192, 168, 42, 105), new ModuleCallbacks());
@@ -204,8 +273,10 @@ void setup() {
 /**
  * @brief Loop performs the calibration process or the reading data process.
  * 
- * @details 
- * 
+ * @details It can either perform the calibration function or the reading data procedure.
+ * If the calibrate operation return false for some error, it will be performed a blinking to
+ * signal it.
+ * Once the calibration ends correctly, the reading data operation will start.
  */
 void loop() {
 	
@@ -222,15 +293,14 @@ void loop() {
 					digitalWrite(STM_NOTIFICATION_LED_PIN, HIGH);
 					delay(CAL_ERROR_LED_DELAY_MS);
 				}
-				if(calibrationTries--){
+				if(!calibrationTries--){
 					calibration=false;
 					calibrationTries=CAL_NUM_TRIES;
 				}									
 				delay(CAL_TRIES_DELAY_MS);
 			}
 	}
-	else{
-		readData();
-	}		
+	else
+		readData();	
 	
 }
